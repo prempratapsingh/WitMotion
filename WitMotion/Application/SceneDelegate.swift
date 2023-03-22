d0cac05e6fb2d28c1ca87519b83e762502865199
Prem Pratap Singh <prem.pratap@icloud.com>
2023-03-22 14:58:44 +0530

    Remove main storyboard and update project structure

--- WitMotion/Application/SceneDelegate.swift
+++ WitMotion/Application/SceneDelegate.swift
@@ -11,12 +11,22 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        self.window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        self.window?.windowScene = windowScene
+        self.presentMainViewController()
+    }
+    
+    /// Instantiates MainViewController and set it as the rootViewController
+    private func presentMainViewController() {
+        let mainViewController =  MainViewController()
+        self.window?.rootViewController = mainViewController
+        self.window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -46,7 +56,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
 }
 
--- WitMotion/Views/MainView/MainViewController.swift
+++ WitMotion/Views/MainView/MainViewController.swift
@@ -1,5 +1,6 @@
+
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  WitMotion
 //
 //  Created by Prem Pratap Singh on 22/03/23.
@@ -7,13 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBlue
     }
-
-
 }
 
