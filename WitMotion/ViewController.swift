61b5cba22b81d542248265481e06512a3ce08a49
Prem Pratap Singh <prem.pratap@icloud.com>
2023-03-22 12:43:31 +0530

    Add initial Xcode project structure

---
+++ WitMotion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WitMotion
+//
+//  Created by Prem Pratap Singh on 22/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
